b4977837570d7a1b08fee69a4dc54fed02bdbe24
ceesvanveluw <ceesvanveluw@gmail.com>
2026-08-17 11:45:26 +0200

    Fetch and print opportunity description from API

--- sam_test.py
+++ sam_test.py
@@ -1,5 +1,34 @@
+import os
+import requests
+
+api_key = os.environ["SAM_API_KEY"]
+
+url = "https://api.sam.gov/opportunities/v2/search"
+
+params = {
+    "api_key": api_key,
+    "limit": 1,
+    "postedFrom": "08/01/2026",
+    "postedTo": "08/17/2026"
+}
+
+response = requests.get(url, params=params)
+data = response.json()
+
+first = data["opportunitiesData"][0]
+
 desc_url = first["description"]
 
-response = requests.get(desc_url)
+print("DESCRIPTION URL")
+print(desc_url)
 
-print(response.text[:3000])
+print()
+print("DOWNLOADING DESCRIPTION...")
+print()
+
+desc_response = requests.get(desc_url)
+
+print("STATUS:", desc_response.status_code)
+
+print()
+print(desc_response.text[:5000])
