5f409015a5499fdb2cf1e29820df391bca887e4b
ceesvanveluw <ceesvanveluw@gmail.com>
2026-08-17 11:44:30 +0200

    Refactor sam_test.py to streamline API request
    Removed unused imports and simplified API call.

--- sam_test.py
+++ sam_test.py
@@ -1,19 +1,5 @@
-import os
-import requests
-import json
+desc_url = first["description"]
 
-api_key = os.environ["SAM_API_KEY"]
+response = requests.get(desc_url)
 
-url = "https://api.sam.gov/opportunities/v2/search"
-
-params = {
-    "api_key": api_key,
-    "limit": 1,
-    "postedFrom": "08/01/2026",
-    "postedTo": "08/17/2026"
-}
-
-response = requests.get(url, params=params)
-data = response.json()
-
-print(json.dumps(data["opportunitiesData"][0], indent=2))
+print(response.text[:3000])
