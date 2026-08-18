99130f5fb8a39c7f253d8c1fee55be1eccfdb810
ceesvanveluw <ceesvanveluw@gmail.com>
2026-08-18 07:50:18 +0200

    changed chrome

--- canada_test.py
+++ canada_test.py
@@ -11,8 +11,11 @@ headers = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/139.0 Safari/537.36"
-    )
+        "Chrome/138.0.0.0 Safari/537.36"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-CA,en;q=0.9",
+    "Referer": "https://canadabuys.canada.ca/",
 }
 
 SEARCH_WORDS = [
